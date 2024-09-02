db1f8a0083ec3b2d42c652db1614e6fd54c0cbce
Navid Boloorian <navidboloorian0@gmail.com>
2024-09-01 23:50:13 -0700

    boilerplate

---
+++ client/src/app/room-form/room-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wh-room-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './room-form.component.html',
+  styleUrl: './room-form.component.scss'
+})
+export class RoomFormComponent {
+
+}
