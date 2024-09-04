e1cf715b7bb50851bc4e0954f21008abd5784b34
Navid Boloorian <navidboloorian0@gmail.com>
2024-09-03 19:01:11 -0700

    made room scaffolding

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RoomComponent } from './room/room.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    title: 'Wormhole',
+  },
+  {
+    path: 'room',
+    component: RoomComponent,
     title: 'Wormhole',
   },
 ];
---
+++ client/src/app/room/room.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'wh-room',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './room.component.html',
+  styleUrl: './room.component.scss',
+})
+export class RoomComponent {}
