51cfe8c2a45e41e3fdbfa88dee6d999f53a06059
Navid Boloorian <navidboloorian0@gmail.com>
2024-09-07 17:26:09 -0700

    local video up and running

--- client/src/app/call.service.ts
+++ client/src/app/call.service.ts
@@ -1,6 +1,26 @@
-import { Injectable } from '@angular/core';
+import { ElementRef, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CallService {}
+export class CallService {
+  localVideo: ElementRef;
+
+  constructor() {}
+
+  async initLocalVideo(localVideo: ElementRef) {
+    this.localVideo = localVideo;
+    const videoElement = localVideo.nativeElement as HTMLVideoElement;
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      videoElement.srcObject = stream;
+      videoElement.play();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
--- client/src/app/room/room.component.ts
+++ client/src/app/room/room.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CallService } from '../call.service';
 
 @Component({
   selector: 'wh-room',
@@ -8,4 +9,12 @@ import { RouterModule } from '@angular/router';
   templateUrl: './room.component.html',
   styleUrl: './room.component.scss',
 })
-export class RoomComponent {}
+export class RoomComponent implements AfterViewInit {
+  @ViewChild('localVideo') localVideo: ElementRef;
+
+  constructor(private callService: CallService) {}
+
+  ngAfterViewInit(): void {
+    this.callService.initLocalVideo(this.localVideo);
+  }
+}
